477e00bb85e40bc2748d8fe7c434a8997accb512
Mykola Kruliv <mykolakruliv@gmail.com>
2019-05-14 15:21:06 +0300

    Change app location

--- Scripts/appdeploy.sh
+++ Scripts/appdeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 yum install java-1.8.0-openjdk wget nano -y
 cd /vagrant
-wget https://www.dropbox.com/s/siq3xtvj2hmzjz4/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar?dl=0
-mv spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar?dl=0 spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar
-chown vagrant:vagrant spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar
-nohup java -jar /vagrant/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar -Dspring.profiles.active=mysql  > /dev/null 2>&1 & 
+#wget https://www.dropbox.com/s/siq3xtvj2hmzjz4/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar?dl=0
+#mv spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar?dl=0 spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar
+chown vagrant:vagrant APP/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar
+nohup java -jar /vagrant/APP/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar -Dspring.profiles.active=mysql  > /dev/null 2>&1 & 
